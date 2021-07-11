be9cc6470166ca09ef98326f9756d59392dd3b74
Josep <josepdal97@gmail.com>
2021-07-11 02:13:53 +0200

    favicon and description image

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'twitterimage',
+        name: 'twitter:image',
+        content: 'https://lto.stakely.io/lto-description-image.jpg'
+      },
+      {
+        hid: 'ogimage',
+        property: 'og:image',
+        content: 'https://lto.stakely.io/lto-description-image.jpg'
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
